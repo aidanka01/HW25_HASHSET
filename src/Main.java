55aedd63b5bd71be4d5449f771169fe6ebd04ccc
Aidana <108028245+aidanka01@users.noreply.github.com>
2022-08-14 10:44:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /**   Эки setтин симметриялык айырмасын эсептеген symmetricDifference методун ишке ашырыныз.
+         Метод натыйжаны жаңы set катары кайтарышы керек.
+         Методко параметр катары берилген setтерди өзгөртүүгө жол берилбейт.
+         Мисалы: [1, 2, 3} жана {0, 1, 2]
+         Жооп: [0, 3]
+         public static Set<Integer> symmetricDifference (Set<Integer> set1, Set<Integer> set2)
+         методу болушу керек
+         */
+
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(1996);
+        set1.add(1999);
+        set1.add(2000);
+        set1.add(2002);
+        System.out.println("Set1: " + set1);
+
+        Set<Integer> set2 = new LinkedHashSet<>();
+        set2.add(1997);
+        set2.add(2002);
+        set2.add(1998);
+        set2.add(1996);
+        System.out.println("Set2: " + set2);
+
+
+        System.out.println("Symmetric difference of Set1: " + difference(set1, set2));
+        System.out.println("Symmetric difference of Set2: " + difference2(set1,set2));
+
+
+    }
+
+    public static <Integer> Set<Integer> difference(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> symmetricDiff = new HashSet<>(set1);
+        symmetricDiff.removeAll(set2);
+        return symmetricDiff;
+    }
+
+    public static <Integer> Set<Integer> difference2(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> symmetricDiff = new HashSet<>(set2);
+        symmetricDiff.removeAll(set1);
+        return symmetricDiff;
+    }
+
+}
+
